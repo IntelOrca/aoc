59d63f5f097eeadad4005265498fd1b8b583b0d0
Ted John <ted@brambles.org>
2023-12-04 22:39:58 +0000

    Update solution for 2023/04

--- 2023/04.fs
+++ 2023/04.fs
@@ -46,25 +46,43 @@ let getScore card =
 let findCard n cards =
     cards |> List.tryFind (fun x -> x.number = n)
 
-let rec processGames score cards =
+let takeSimilar cards =
+    let rec takeSimilarInner acc cards =
+        let last = List.head acc
+        match cards with
+        | head :: tail when head.number = last.number ->
+            takeSimilarInner (head :: acc) tail
+        | _ -> (acc, cards)
     match cards with
-    | head :: tail ->
-        let count = getNumWinners head
-        let cardNumbers = List.init count (fun i -> head.number + i + 1)
-        let newCards =
-            cardNumbers
-            |> List.choose (fun n -> findCard n cards)
-            |> List.append tail
-            |> List.sortBy (fun c -> c.number)
-        processGames (score + 1) newCards
+    | head :: tail -> takeSimilarInner [head] tail
+    | [] -> ([], [])
+
+module List =
+    let multiply multiplier lst =
+        (fun _ -> lst)
+        |> List.init multiplier
+        |> List.collect id
+
+let rec processGames score cards =
+    let (similar, remaining) = takeSimilar cards
+    match similar with
     | [] -> score
+    | head :: _ ->
+        let multiplier = List.length similar
+        let count = getNumWinners head
+        let newCards =
+            List.init count (fun i -> head.number + i + 1)
+            |> List.choose (fun n -> findCard n cards)
+            |> List.multiply multiplier
+            |> List.append remaining
+            |> List.sortBy (fun c -> c.number)
+        processGames (score + multiplier) newCards
 
 let solvePart1 = List.map getScore >> List.sum
-
 let solvePart2 cards = processGames 0 cards
 
 [<aoc.Solution(2023, 4)>]
 let day04 () =
-    let cards = readInput "data/2023/input04.txt"
+    let cards = readInput "data/2023/sample04.txt"
     cards |> solvePart1 |> printfn "%A"
     cards |> solvePart2 |> printfn "%A"
