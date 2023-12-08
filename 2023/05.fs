162580fcb7519ef718805a1ba37cd8e48b335c7e
Ted John <ted@brambles.org>
2023-12-08 01:11:57 +0000

    Update solution for 2023/05

--- 2023/05.fs
+++ 2023/05.fs
@@ -1,12 +1,7 @@
 module aoc_2023_05
 
-type Remap = {
-    dst: int64
-    src: int64
-    len: int64
-}
-
-type Transformer = int64 -> int64
+type Range = int64 * int64
+type Transformer = Range list -> Range list
 type Config = {
     seeds: int64 list
     transformer: Transformer
@@ -39,30 +34,60 @@ let rec parse acc lines =
         parse (section :: acc) remaining
     | _ -> acc
 
+let createRange s e = (s, e - s)
+let intersectRange (tStart, tLen) (inStart, inLen) =
+    let tEnd = tStart + tLen
+    let inEnd = inStart + inLen
+    if inEnd <= tStart || inStart >= tEnd then
+        ([createRange inStart inEnd], [])
+    elif inStart < tStart && inEnd > tStart && inEnd <= tEnd then
+        ([createRange inStart tStart], [createRange tStart tEnd])
+    elif inStart >= tStart && inStart < tEnd && inEnd > tEnd then
+        ([createRange tEnd inEnd], [createRange inStart tEnd])
+    elif inStart < tStart && inEnd > tEnd then
+        ([createRange inStart tStart; createRange tEnd inEnd], [createRange tStart tEnd])
+    else
+        ([], [createRange inStart inEnd])
+
 let createSubTransformer input =
     match input with
     | [dst; src; len] ->
-        fun i ->
-            if i >= src && i < src + len then
-                Some (dst + (i - src))
-            else
-                None
-    | _ -> fun _ -> None
+        let transformRange (rStart, rLen) = (dst + (rStart - src), rLen)
+        fun ranges ->
+            match ranges with
+            | [] -> ([], [])
+            | _ ->
+                let (unmapped, mapped) =
+                    ranges
+                    |> List.map (intersectRange (src, len))
+                    |> List.reduce (fun (u0, m0) (u1, m1) ->
+                        (u0 @ u1, m0 @ m1))
+                let transformed =
+                    mapped
+                    |> List.map transformRange
+                (unmapped, transformed)
+    | _ -> failwith "Invalid input for transformer"
 
 let createTransformer input =
-    let subTransformers =
+    let rec foldTransformers mapped unmapped transformers =
+        match transformers with
+        | head :: tail ->
+            let (unmapped2, mapped2) = head unmapped
+            foldTransformers (mapped @ mapped2) unmapped2 tail
+        | [] ->
+            mapped @ unmapped
+
+    let transformers =
         input
         |> List.map createSubTransformer
-        |> List.append [fun i -> Some i]
-    fun i ->
-        subTransformers
-        |> List.pick (fun t -> t i)
+
+    fun unmapped -> foldTransformers [] unmapped transformers
 
 let createRootTransformer input =
-    fun i ->
+    fun unmapped ->
         input
         |> List.map createTransformer
-        |> List.fold (fun j f -> f j) i
+        |> List.fold (fun unmapped f -> f unmapped) unmapped
 
 let getConfig sections =
     let seeds =
@@ -87,14 +112,27 @@ let readInput =
 
 let solvePart1 config =
     config.seeds
-    |> List.map config.transformer
+    |> List.map (fun i -> [(i, 1L)])
+    |> List.collect config.transformer
+    |> List.map fst
     |> List.min
 
 let solvePart2 config =
-    solvePart1 config
+    config.seeds
+    |> List.chunkBySize 2
+    |> List.map (function
+        | [a; b] -> [(a, b)]
+        | _ -> [])
+    |> List.collect config.transformer
+    |> List.map fst
+    |> List.min
+
+let solve input =
+    let config = readInput input
+    config |> solvePart1 |> printfn "%d"
+    config |> solvePart2 |> printfn "%d"
 
 [<aoc.Solution(2023, 5)>]
 let day05 () =
-    let config = readInput "data/2023/sample05.txt"
-    config |> solvePart1 |> printfn "%d"
-    config |> solvePart2 |> printfn "%d"
+    solve "data/2023/sample05.txt"
+    solve "data/2023/input05.txt"
